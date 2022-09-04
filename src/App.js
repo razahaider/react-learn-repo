04cf7ba411d72924ce7a6ec12413edcbe9f5e364
Haider Raza <sayedhaiderr@gmail.com>
2022-09-05 01:06:19 +0530

    initial files starter from course

--- src/App.js
+++ src/App.js
@@ -1,79 +1,57 @@
-import { useState } from 'react'
-import Chart from './components/ExpensesChart/Chart'
-import ExpenseFilter from './components/ExpenseUtils/ExpenseFilter'
-import ExpenseItem from './components/Expenses/ExpenseItem'
-import Expenses from './components/Expenses/Expenses'
-import ExpensesChart from './components/Expenses/ExpensesChart'
-import IntroductionTitle from './components/IntroductionTitle'
-import NewExpense from './components/NewExpense/NewExpense'
+import React, { useState } from 'react';
 
-const DUMMY_DATA = [
-  {
-    title: 'vapes ',
-    amount: 300,
-    date: new Date(2020, 9, 12)
-  },
-  {
-    title: 'mandi chicken',
-    amount: 350,
-    date: new Date(2020, 2, 12)
-  },
-  {
-    title: 'vapes ',
-    amount: 4500,
-    date: new Date(2021, 9, 12)
-  },
-  {
-    title: 'mandi chicken',
-    amount: 220,
-    date: new Date(2021, 2, 12)
-  },
-  {
-    title: 'Car Insurance',
-    amount: 123,
-    date: new Date(2021, 10, 12)
-  },
-  {
-    title: 'Groceries',
-    amount: 456,
-    date: new Date(2021, 11, 5)
-  },
-  {
-    title: 'Travel',
-    amount: 789,
-    date: new Date(2021, 11, 4)
+import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import './App.css';
+
+const App = () => {
+  const [courseGoals, setCourseGoals] = useState([
+    { text: 'Do all exercises!', id: 'g1' },
+    { text: 'Finish the course!', id: 'g2' }
+  ]);
+
+  const addGoalHandler = enteredText => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const deleteItemHandler = goalId => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
   }
-]
-function App () {
-  const [expenseData, setExpenseData] = useState(DUMMY_DATA)
-  const newExpenseDataHandler = newExpenseData => {
-    //expenseData.values = { ...newExpenseData }
-    //since this above line of code modifies expenseData so our expectation is this modified data will show in Expenses component
-    //but in react refresh reload of components wont happen becos of static variable modification/updation
-    //thts why this data needs to be update in State hooks and state hooks vars passed into components(since they re renders component on stat change
-    return setExpenseData(prevState => [newExpenseData, ...prevState])
-  }
-  console.log('new expensedata updated: ')
-  console.log(expenseData)
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <IntroductionTitle />
-        <NewExpense getDataAppNewExpenseData={newExpenseDataHandler} />
-
-        <Expenses expenses={expenseData} />
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id="goals">
+        {content}
+        {/* {courseGoals.length > 0 && (
+          <CourseGoalList
+            items={courseGoals}
+            onDeleteItem={deleteItemHandler}
+          />
+        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+        } */}
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
